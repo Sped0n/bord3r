112572bb8f7310267f1451766771574643e1377d
GitButler <gitbutler@gitbutler.com>
2024-07-12 00:04:09 +0800

    GitButler WIP Commit

--- davinci.py
+++ davinci.py
@@ -1,6 +1,3 @@
-from fractions import Fraction
-from math import ceil
-
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from PIL.Image import Image as PILImage
 from PIL.ImageDraw import ImageDraw as PILDraw
@@ -24,7 +21,7 @@ class Davinci:
         if self.__img.size[0] >= self.__img.size[1]:
             self.__vertical: bool = False
         else:
-            self.__vertical: bool = True
+            self.__vertical = True
             self.__img = self.__img.rotate(-90, expand=True)
         self.__original_width, self.__original_height = self.__img.size
         # short and long side of the image
@@ -33,7 +30,7 @@ class Davinci:
         self.__long_side: int = self.__original_width
         self.__short_side: int = self.__original_height
         # half side length of the expanded square, used as a scale for watermark
-        self.__hsl = int((self.__long_side + 0.2 * self.__short_side) / 2)
+        self.__hsl = int((self.__long_side + 0.15 * self.__short_side) / 2)
         # hortizontal and vertical fill
         hor_fill: int = int(self.__hsl - self.__original_width / 2)
         ver_fill: int = int(self.__hsl - self.__original_height / 2)
@@ -52,70 +49,43 @@ class Davinci:
         self.__draw: PILDraw = ImageDraw.Draw(self.__plate)
         # fonts
         self.__font_size: int = int(0.04 * self.__hsl)
-        self.__font = ImageFont.truetype(
-            "./fonts/JetBrainsMono-Medium.ttf", self.__font_size
-        )
-        self.__info_font = ImageFont.truetype(
-            "./fonts/JetBrainsMono-LightItalic.ttf", int(self.__font_size * 0.9)
-        )
-        self.__artist_font = ImageFont.truetype(
-            "./fonts/JetBrainsMono-LightItalic.ttf", int(self.__font_size * 1.1)
-        )
+        self.__font = ImageFont.truetype("./fonts/Geist-Medium.ttf", self.__font_size)
 
     def __add_capture_time(self) -> None:
         # capture time, on top left corner
         text: str = str(self.__exif["EXIF:CreateDate"])
         x: int = int(self.__hsl - self.__original_width / 2)
-        y: int = int(self.__hsl - self.__original_height / 2 - 0.017 * self.__hsl)
+        y: int = int(self.__hsl - self.__original_height / 2 - 0.018 * self.__hsl)
         self.__draw.text((x, y), text, anchor="lb", font=self.__font, fill="black")
 
     def __add_location(self) -> None:
         # location, on top right corner
         text: str = self.__location
         x: int = int(self.__hsl + self.__original_width / 2)
-        y: int = int(self.__hsl - self.__original_height / 2 - 0.013 * self.__hsl)
+        y: int = int(self.__hsl - self.__original_height / 2 - 0.015 * self.__hsl)
         self.__draw.text((x, y), text, anchor="rb", font=self.__font, fill="black")
 
     def __add_camera_model(self) -> None:
         # camera model, on bottom left corner row 1
         text: str = str(self.__exif["EXIF:Make"]) + " " + str(self.__exif["EXIF:Model"])
         x: int = int(self.__hsl - self.__original_width / 2)
-        y: int = int(self.__hsl + self.__original_height / 2 + 0.015 * self.__hsl)
+        y: int = int(self.__hsl + self.__original_height / 2 + 0.018 * self.__hsl)
         self.__draw.text((x, y), text, anchor="lt", font=self.__font, fill="black")
-
-    def __add_camera_params(self) -> None:
-        # capture param, on bottom left corner row 2
-        iso: str = "ISO" + str(self.__exif["EXIF:ISO"])
-        aperture: str = "f/" + str(ceil(float(self.__exif["EXIF:ApertureValue"])))
-        exposure_time: str = (
-            str(Fraction(float(self.__exif["EXIF:ExposureTime"])).limit_denominator())
-            + "s"
-        )
-        focal_length: str = "Φ" + str(self.__exif["EXIF:FocalLength"]) + "mm"
-        text: str = focal_length + " " + iso + " " + aperture + " " + exposure_time
-        x: int = int(self.__hsl - self.__original_width / 2)
-        y: int = int(
-            self.__hsl
-            + self.__original_height / 2
-            + self.__font_size
-            + 0.021 * self.__hsl
-        )
-        self.__draw.text((x, y), text, anchor="lt", font=self.__info_font, fill="black")
 
     def __add_artist(self) -> None:
         # artist, on bottom right corner
-        text = str(self.__exif["EXIF:Artist"])
-        x = int(self.__hsl + self.__original_width / 2 - 0.012 * self.__hsl)
-        y = int(self.__hsl + self.__original_height / 2 + 0.027 * self.__hsl)
-        self.__draw.text(
-            (x, y), text, anchor="rt", font=self.__artist_font, fill="black"
-        )
+        try:
+            text = str(self.__exif["EXIF:Artist"])
+        except KeyError:
+            text = "@sped0n"
+        x = int(self.__hsl + self.__original_width / 2 - 0.007 * self.__hsl)
+        y: int = int(self.__hsl + self.__original_height / 2 + 0.016 * self.__hsl)
+        self.__draw.text((x, y), text, anchor="rt", font=self.__font, fill="black")
 
     def process(self) -> PILImage:
         self.__add_capture_time()
         self.__add_location()
         self.__add_camera_model()
-        self.__add_camera_params()
         self.__add_artist()
         if self.__vertical:
             return self.__plate.rotate(90, expand=True)
